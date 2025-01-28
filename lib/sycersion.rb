2a4c85ec27f4376bb1a1c1ba6f2be0db1bfb0efc
Pierre Sugar <pierre@sugaryourcoffee.de>
2025-01-28 23:49:56 +0100

    refactor

--- lib/sycersion.rb
+++ lib/sycersion.rb
@@ -6,6 +6,7 @@ require_relative 'sycersion/version_environment'
 require_relative 'sycersion/version_setter'
 require_relative 'sycersion/version_incrementer'
 require_relative 'sycersion/version_info'
+require_relative 'sycersion/semver'
 
 module Sycersion
   class Error < StandardError; end
@@ -14,21 +15,34 @@ module Sycersion
   class Runner
     def initialize(argv)
       @options = Options.new(argv).options
-      pp @options
+      pp @options if ENV['SYC_DEBUGW']
     end
 
     def run
-      if @options[:init]
-        Sycersion::VersionEnvironment.new.setup
-      elsif @options[:set]
+      environment_settings
+      version_manipulation
+      configuration_information
+    end
+
+    def environment_settings
+      Sycersion::VersionEnvironment.new.setup if @options[:init]
+    end
+
+    def version_manipulation
+      case @options
+      when :set
         Sycersion::VersionSetter.new.version = (@options[:set])
-      elsif @options[:suffix]
-        Sycersion::VersionSetter.new.suffix = (@options[:suffix])
-      elsif @options[:inc]
+      when :pre_release
+        Sycersion::VersionSetter.new.pre_release = (@options[:pre_release])
+      when :build
+        Sycersion::VersionSetter.new.build = (@options[:build])
+      when :inc
         Sycersion::VersionIncrementer.new.increment(@options[:inc])
-      elsif @options[:info]
-        puts Sycersion::VersionInfo.new.process(@options)
       end
+    end
+
+    def configuration_information
+      puts Sycersion::VersionInfo.new.process(@options) if @options[:info]
     end
   end
 end
